4095af5223e2ea310c095622843600c835e0c3cd
Benjamin Sengupta <benjaminsengupta@gmail.com>
2022-02-25 19:50:14 -0500

    Add absolute path to python script

--- .github/workflows/yaml-to-json.py
+++ .github/workflows/yaml-to-json.py
@@ -1,18 +1,26 @@
 import sys
 import yaml
 import json
+from os import path
+
 
 def main(argv):
-	if len(argv) < 2:
-		print("Error: Expected input YAML file")
-		print("Usage: python yaml-to-json.py <yaml input file> <json output file>")
-		sys.exit(1)
-	if len(argv) < 3:
-		print("Error: Expected output JSON file")
-		print("Usage: python yaml-to-json.py <yaml input file> <json output file>")
-		sys.exit(1)
-	with open(argv[1], 'r') as yaml_in, open(argv[2], "w") as json_out:
-		yaml_object = yaml.safe_load(yaml_in)
-		json.dump(yaml_object, json_out)
+    if len(argv) < 2:
+        print("Error: Expected input YAML file")
+        print("Usage: python yaml-to-json.py <yaml input file> <json output file>")
+        sys.exit(1)
+    if len(argv) < 3:
+        print("Error: Expected output JSON file")
+        print("Usage: python yaml-to-json.py <yaml input file> <json output file>")
+        sys.exit(1)
+
+    root_dir = path.join(path.dirname(__file__), "../..")
+    yaml_file_path = path.join(root_dir, argv[1])
+    json_file_path = path.join(root_dir, argv[2])
+
+    with open(yaml_file_path, "r") as yaml_in, open(json_file_path, "w") as json_out:
+        yaml_object = yaml.safe_load(yaml_in)
+        json.dump(yaml_object, json_out)
+
 
 main(sys.argv)
