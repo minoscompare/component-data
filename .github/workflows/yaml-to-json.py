2c2af4dbf05e8d4b6640f2243a0a1c59c3346178
Benjamin Sengupta <benjaminsengupta@gmail.com>
2022-02-25 19:58:32 -0500

    Test github actions

--- .github/workflows/yaml-to-json.py
+++ .github/workflows/yaml-to-json.py
@@ -1,7 +1,6 @@
 import sys
 import yaml
 import json
-from os import path
 
 
 def main(argv):
@@ -13,12 +12,7 @@ def main(argv):
         print("Error: Expected output JSON file")
         print("Usage: python yaml-to-json.py <yaml input file> <json output file>")
         sys.exit(1)
-
-    root_dir = path.abspath(path.join(path.dirname(__file__), "../.."))
-    yaml_file_path = path.join(root_dir, argv[1])
-    json_file_path = path.join(root_dir, argv[2])
-
-    with open(yaml_file_path, "r") as yaml_in, open(json_file_path, "w") as json_out:
+    with open(argv[1], "r") as yaml_in, open(argv[2], "w") as json_out:
         yaml_object = yaml.safe_load(yaml_in)
         json.dump(yaml_object, json_out)
 
